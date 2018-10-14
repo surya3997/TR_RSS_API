e5136d4326dd4a399e97c59a39410849adcc4073
Guru Prasath <guru98@live.com>
2018-10-14 20:31:05 +0530

    completed parsing

--- tr_rss_api.py
+++ tr_rss_api.py
@@ -5,6 +5,7 @@ Created on Sun Oct  7 12:40:50 2018
 
 @author: guru
 """
+
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import re
@@ -16,4 +17,18 @@ firstpost = soup.find(class_="ipsType_textblock")
 tnmheader = firstpost.find(string=re.compile("Tamil New Movies"))
 tnm = tnmheader.find_next("strong")
 
-nms = tnm.find_all("br")
+nms = str(tnm).split("<br/>")
+
+movie_list = {}
+
+for nm in nms:
+    nm = BeautifulSoup(nm,'lxml')
+    name = nm.text.split(' - [')[0]
+    links = {}
+    for qual in nm.find_all('a'):
+        qual_link = qual.attrs['href']
+        qual_name = qual.text
+        links[qual_name] = qual_link
+        
+    movie_list[name]=links
+    
